d0ba908dd5a7d03318d6b093f7ba11261b591031
Sam Vredenburgh <sam.vburgh@gmail.com>
2025-08-14 12:55:57 -0600

    fix dashboard test file for ci run

--- monitoring/test_dashboard.py
+++ monitoring/test_dashboard.py
@@ -1,5 +1,5 @@
 from streamlit.testing.v1 import AppTest
 
 def test_app_runs():
-    at = AppTest.from_file("monitoring/app.py").run()
+    at = AppTest.from_file("./app.py").run()
     assert len(at.exception) == 0
